6fa59d63a6a010dabfa16109bdc88b7659ce6f88
Willians <williansvf@hotmail.com>
2023-02-22 17:44:45 -0300

    inserindo CustomerCreatedEvent no handler

--- src/domain/@shared/event/product/handler/send-email-when-product-is-created.handler.ts
+++ src/domain/@shared/event/product/handler/send-email-when-product-is-created.handler.ts
@@ -1,8 +1,9 @@
 import EventHandlerInterface from "../../event-handler.interface";
 import eventInterface from "../../event.interface";
+import ProductCreatedEvent from "../product-created.event";
 
 export default class SendEmailWhenProductIsCreatedHandler implements EventHandlerInterface{
-    handler(event: eventInterface): void {
+    handler(event: ProductCreatedEvent): void {
         console.log('Sendind email to....')
     }
 
