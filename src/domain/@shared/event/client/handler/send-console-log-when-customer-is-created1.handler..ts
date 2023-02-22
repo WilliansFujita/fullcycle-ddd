96180b28ff2ffb6962aa23fc7304ac547521ba69
Willians <williansvf@hotmail.com>
2023-02-22 17:44:38 -0300

    inserindo os eventos no handler

--- src/domain/@shared/event/client/handler/send-console-log-when-customer-is-created1.handler..ts
+++ src/domain/@shared/event/client/handler/send-console-log-when-customer-is-created1.handler..ts
@@ -1,9 +1,10 @@
 import EventHandlerInterface from "../../event-handler.interface";
 import eventInterface from "../../event.interface";
+import CustomerCreatedEvent from "../customer-created.event";
 
 export default class SendConsoleWhenCustomerIsCreatedHandler1 implements EventHandlerInterface{
 
-    handler(event: eventInterface): void {
+    handler(event: CustomerCreatedEvent): void {
         console.log('Esse é o primeiro console.log do evento: CustomerCreated')
     }
 
